65a464d0e81c8f2dc31e78c2c226c5c993a7cf9b
Glan_Sheep <kopkcgungun@gmail.com>
2023-03-03 11:37:26 +0900

    Update interactionCreate.ts
    エラー制御の追加

--- events/interactionCreate.ts
+++ events/interactionCreate.ts
@@ -4,15 +4,14 @@ import {
   InteractionResponseTypes,
   InteractionTypes,
 } from "discordeno/mod.ts";
-import { EmbedBuilder } from "../lib/embed.ts";
-import log from "../utils/logger.ts";
+import { EmbedBuilder } from "lib/embed.ts";
+import log from "utils/logger.ts";
 
 BotName.events.interactionCreate = (_, interaction) => {
   if (!interaction.data) return;
 
   switch (interaction.type) {
     case InteractionTypes.ApplicationCommand: {
-      log.info(`[Application Command] ${interaction.data.name} command`);
       const args = interaction.data.options;
       if (!args) {
         const reply = BotName.commands
@@ -30,22 +29,36 @@ BotName.events.interactionCreate = (_, interaction) => {
   }
 };
 
+const error_embeds = new EmbedBuilder()
+  .setTitle("⚠️ERROR⚠️").setDescription(`予期せぬエラーが発生しました。
+オプションを確認し、
+時間を置いて再度試してください。`);
+
 function response(
   reply: EmbedBuilder | Promise<EmbedBuilder> | undefined,
   interaction: Interaction
 ) {
   if (reply instanceof Promise<EmbedBuilder>) {
     reply.then((embed) => {
+      BotName.helpers
+        .sendInteractionResponse(interaction.id, interaction.token, {
+          type: InteractionResponseTypes.ChannelMessageWithSource,
+          data: {
+            embeds: [embed.data],
+          },
+        })
+    }).catch((error) => {
       BotName.helpers.sendInteractionResponse(
         interaction.id,
         interaction.token,
         {
           type: InteractionResponseTypes.ChannelMessageWithSource,
           data: {
-            embeds: [embed.data],
+            embeds: [error_embeds.data],
           },
         }
       );
+      log.error(error);
     });
   } else if (reply instanceof EmbedBuilder) {
     BotName.helpers.sendInteractionResponse(interaction.id, interaction.token, {
@@ -54,5 +67,13 @@ function response(
         embeds: [reply.data],
       },
     });
+  } else {
+    BotName.helpers.sendInteractionResponse(interaction.id, interaction.token, {
+      type: InteractionResponseTypes.ChannelMessageWithSource,
+      data: {
+        embeds: [error_embeds.data],
+      },
+    });
+    log.error("(response) undefined error")
   }
 }
